a2cbfef59257f3cbbcb0a85ed446aa49772368bb
sirsatshraddha12-bit <sirsatshraddha12@gmail.com>
2026-05-16 23:26:37 +0530

    done..

--- Array.js
+++ Array.js
@@ -17,3 +17,14 @@ const another_array=[1,2,3,[4,5,6]7,[6,7,[4,5]]]
 const real_another_array=another_array.flat(Infinity)
 console.log(real_another_array);          //[1,2,3,4,5,6,7,6,7,4,5]
 
+console.log(Array.isArray("Shraddha"));   //false
+console.log(Array.from("Shraddha"));
+
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3));     //[100,200,300]
+
+
