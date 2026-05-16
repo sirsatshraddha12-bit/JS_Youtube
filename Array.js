57bd246f8d46a60447bc25e039ce4ff6249c9286
sirsatshraddha12-bit <sirsatshraddha12@gmail.com>
2026-05-16 23:00:26 +0530

    array methos done..

--- Array.js
+++ Array.js
@@ -1,34 +1,19 @@
-//Array
+const marvel_heroes=["thor","Ironman","Spiderman"]
+const marvel_heroes=["Superman","Flash","Batman"]
 
-const myArr=[0,1,2,3,4,5]
-const myHeros=["Shaktiman","Superman"]
-const myArr2=new Array(1,2,3,4)
-//console.log(myArr[1]);
+marvel_heros.push(dc_heros)
 
-//methods
+console.log(marvel_heros);                  //["thor","Ironman","Spiderman",["Superman","Flash","Batman"]]
+console.log(marvel_heros[3][1]);           //flash
 
-myArr.push(6)           //add6
-myArr.pop(6)                 //delete 6
-myArr.unshift(9)          //90123456
-myArr.shift()     //1,2,3,4,5
+const allheros=marvel_heros.concat(dc_heros)       //["thor","Ironman","Spiderman","Superman","Flash","Batman"]
+console.log(allheros);                  //["thor","Ironman","Spiderman","Superman","Flash","Batman"]
 
-console.log(myArr.includes(9));        //true
+const all_new_heros=[...marvel_heros, ...dc_heros]
+console.log(all_new_heros);              //["thor","Ironman","Spiderman","Superman","Flash","Batman"]
 
-console.log(myArr.indexOf(9));       //1
+const another_array=[1,2,3,[4,5,6]7,[6,7,[4,5]]]
 
-const newArr=myArr.join()
-console.log(newArr); // "9,0,1,2,3,4,5"
+const real_another_array=another_array.flat(Infinity)
+console.log(real_another_array);          //[1,2,3,4,5,6,7,6,7,4,5]
 
-console.log(myArr); // [9, 0, 1, 2, 3, 4, 5]
-
-
-//slice.splice
-
-console.log("A",myarr);                //A [0,1,2,3,4,5]
-const myn1=myArr.slice(1,3)           
-console.log(myn1);                      //[1,2]
-console.log("B",myArr);                 //B [ 0, 1, 2, 3, 4, 5]
-
-const myn2=myArr.splice(1,3)
-console.log("C",myArr);           //C[0,4,5]
-console.log(myn2);                  //[1,2,3]
